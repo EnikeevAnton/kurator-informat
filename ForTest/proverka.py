0c7aa9b23c3d3b96901d477928cdac20635ab714
EnikeevAnton <139821266+EnikeevAnton@users.noreply.github.com>
2023-07-18 21:35:21 +0300

    Delete ForTest directory

--- ForTest/proverka.py
+++
@@ -1,7 +0,0 @@
-def f(c,m,t):
-    if c>=78: return m%2 == 0
-    if m == 0: return 0
-    h = [f(c+1,m-1,t + 1), f(c+4, m - 1, t + 1), f(c*4, m - 1, t + 1)]
-    return any(h) if m%2 !=0 else all(h)
-#print('19', [s for s in range(1,78) if f(s, 2, [])])
-print([s for s in range(1,78) if f(s,9, 0) and not f(s, 7, 0)])
